caa3f81ce2a41c32f86e150ca829cb5335066a38
mayowaagbi <agbimayowa3@gmail.com>
2024-09-10 14:39:40 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Compnents/Navbar";
+import "./index.scss";
+import "./layout.scss";
+function App() {
+  return (
+    <>
+      <div className="Layout">
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
