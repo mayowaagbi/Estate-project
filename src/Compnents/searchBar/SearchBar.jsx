b3348f614fb4127043fe1ef6632c8d90e2d52533
mayowaagbi <agbimayowa3@gmail.com>
2024-09-18 16:12:29 +0100

    added  more search bar features

--- src/Compnents/searchBar/SearchBar.jsx
+++ src/Compnents/searchBar/SearchBar.jsx
@@ -1,44 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "../searchBar/searchBar.scss";
-import { useState } from "react";
-const types = ["buy", "rent"];
+
 function SearchBar() {
-  const [queary, setQuery] = useState({
+  const types = ["buy", "rent"];
+  const [query, setQuery] = useState({
     type: "buy",
     location: "",
     minPrice: 0,
     maxPrice: 0,
   });
+
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
+
   return (
-    <div className="SearchBar">
+    <div className="searchBar">
       <div className="type">
-        {types.map((type) => {
-          <button key={type} onClick={() => switchType(type)}>
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
             {type}
-          </button>;
-        })}
-        <button onClick={() => switchType("rent")}>Rent</button>
+          </button>
+        ))}
       </div>
+
       <form>
-        <input type="text" name="locaton" id="" placeholder="City Location" />
+        <input
+          type="text"
+          name="location"
+          placeholder="City Location"
+          value={query.location}
+          onChange={(e) =>
+            setQuery((prev) => ({ ...prev, location: e.target.value }))
+          }
+        />
         <input
           type="number"
           name="minPrice"
           min={0}
           max={10000000}
-          id=""
           placeholder="Min price"
+          // value={query.minPrice}
+          // onChange={(e) =>
+          //   setQuery((prev) => ({ ...prev, minPrice: e.target.value }))
+          // }
         />
         <input
           type="number"
           name="maxPrice"
           min={0}
           max={10000000}
-          id=""
           placeholder="Max price"
+          // value={query.maxPrice}
+          // onChange={(e) =>
+          //   setQuery((prev) => ({ ...prev, maxPrice: e.target.value }))
+          //}
         />
-        <button>
-          <img src="/search.png" alt="" />
+        <button type="submit">
+          <img src="/search.png" alt="search" />
         </button>
       </form>
     </div>
